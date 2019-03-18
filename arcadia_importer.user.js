62a0bc6d4b0fb9d99230f6d3903981baefe7ef23
brianfreud <brian.brianschweitzer@gmail.com>
2019-03-18 09:27:09 -0400

    Update arcadia_importer.user.js

--- arcadia_importer.user.js
+++ arcadia_importer.user.js
@@ -10,7 +10,7 @@
 // @require        https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require        https://raw.githubusercontent.com/brianfreud/Userscripts/master/dict_artists.js
 // @require        https://raw.githubusercontent.com/brianfreud/Userscripts/master/dict_labels.js
-// @require        https://raw.githubusercontent.com/brianfreud/musicbrainz-userscripts/friendlier-makeEditNote/lib/mbimport.js
+// @require        https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/master/lib/mbimport.js
 // @require        https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/master/lib/mbimportstyle.js
 // @icon           https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/master/assets/images/Musicbrainz_import_logo.png
 // ==/UserScript==
