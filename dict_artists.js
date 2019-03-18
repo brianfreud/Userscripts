88c437b371ffe3b56a1902f2509ac17354e3e134
brianfreud <brian.brianschweitzer@gmail.com>
2019-03-18 03:35:18 -0400

    Update dict_artists.js

--- dict_artists.js
+++ dict_artists.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Artist dictionary
-// @version        2019.3.17.1
+// @version        2019.3.17.2
 // @namespace      https://github.com/brianfreud
 // @downloadURL    https://raw.githubusercontent.com/brianfreud/Userscripts/edit/master/dict_artists.js
 // @updateURL      https://raw.githubusercontent.com/brianfreud/Userscripts/edit/master/dict_artists.js
@@ -15,15 +15,19 @@ window.artistDB = {
         'Dieter Birkhof': '24d09a11-303b-439e-b2f3-f944da6314f0',
         'Francis Buzzi': '4da1c339-8e6d-471c-89f7-aaf826833b42',
         'Hank Zimmer': 'a01c40c1-45a0-484e-917b-7f58249a72dc',
+        'Hannes Jaric': '44f87ff9-c256-4f4e-95c2-3fdc27b21034',
         'Howard Young': '46eb9b90-bf3a-41f1-8dfb-6b6f93b3d56b',
         'Isaac Fox': '67c5778a-c30c-42ed-814f-b7697cf2f696',
         'Jeff Goldsmith': '73d18bcc-68b4-45fd-affe-2ccae26101e2',
         'John Snider': 'd2b0f8f1-799a-478e-9102-c3f94a593adf',
+        'Jonathan Wales': '2722d94f-0bd9-44a9-a26d-36d450e8162b',
         'Klaus Back': '53be9b47-1c45-49c9-9fa3-6ee2846c2d33',
+        'Kurt Zöchling': '981e1b89-dbd9-4553-aa26-fc25786155b7',
         'Lane Douglas': 'cab96d11-623a-4c21-92dd-8aad7ae3811a',
         'Larry Hesketh': '0cb80846-a571-44ec-8c5d-1b02c8e00f5a',
         'Linda Turner': '4c43c37c-7867-4015-a065-7bec60b46153',
         'Marvin Bernstein': 'a970ee37-f8ca-4013-b537-6be4d065e6fd',
+        'Maurice Poirot': '91c464d0-f912-457c-a326-43d349b984b8',
         'Max Schneyder': 'd230afdb-0f56-451a-bd74-1d0de5179b28',
         'Michael Jones': '275c7139-c7c3-45b4-96bb-2b49b65932bf',
         'Oliver Shore': '29727e8b-26a6-4363-a51b-a99b53a565bc',
@@ -31,5 +35,7 @@ window.artistDB = {
         'Otakar Olsanik': 'd205fff0-a176-40a5-98aa-b59b25ce084f',
         'Peter Horner': '1fe4b21f-1b95-4093-896a-8da438c62005',
         'Raoul Foster': 'f06fb456-cabd-4231-9a07-90e170571449',
+        'Roman Sillipp': 'ce7ffa98-f716-4883-a7db-62409b18bd26',
+        'Ryszard Szeremeta': '553deb4f-893d-499c-8417-e763b017e23b',
         'Wolf Silver': '88ff3394-6b06-4d1f-a48b-50e71b9fd193'
     };
