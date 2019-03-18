a82e5e910d3044a6e9cefed72b224a358af080b4
brianfreud <brian.brianschweitzer@gmail.com>
2019-03-18 03:15:00 -0400

    Update dict_artists.js

--- dict_artists.js
+++ dict_artists.js
@@ -1,23 +1,35 @@
+// ==UserScript==
+// @name           Artist dictionary
+// @version        2019.3.17.1
+// @namespace      https://github.com/brianfreud
+// @downloadURL    https://raw.githubusercontent.com/brianfreud/Userscripts/edit/master/dict_artists.js
+// @updateURL      https://raw.githubusercontent.com/brianfreud/Userscripts/edit/master/dict_artists.js
+// ==/UserScript==
+
 const artistDB = {
         'Alexander Blechinger': 'e77a8ee6-d6b8-4f36-992b-157f5bc02c45',
-        'Bruce Revell': '',
+        'Art Luft': '959af96a-37c4-4b30-840b-4d91f5b11aff',
+        'Bruce Revell': '81fddc11-f4e8-484d-b7c7-4e92de81f38a',
         'Burt Lankil': 'f1616a67-11ba-42a2-8d8b-a87513f624da',
-        'Dieter Birkhof': '',
-        'Francis Buzzi': '',
-        'Hank Zimmer': '',
-        'Howard Young': '',
-        'Jeff Goldsmith': '',
-        'John Snider': '',
+        'Carlo Andretti': 'bdedb5ca-b71a-4545-91f6-2296c60e009b',
+        'Dieter Birkhof': '24d09a11-303b-439e-b2f3-f944da6314f0',
+        'Francis Buzzi': '4da1c339-8e6d-471c-89f7-aaf826833b42',
+        'Hank Zimmer': 'a01c40c1-45a0-484e-917b-7f58249a72dc',
+        'Howard Young': '46eb9b90-bf3a-41f1-8dfb-6b6f93b3d56b',
+        'Isaac Fox': '67c5778a-c30c-42ed-814f-b7697cf2f696',
+        'Jeff Goldsmith': '73d18bcc-68b4-45fd-affe-2ccae26101e2',
+        'John Snider': 'd2b0f8f1-799a-478e-9102-c3f94a593adf',
         'Klaus Back': '53be9b47-1c45-49c9-9fa3-6ee2846c2d33',
-        'Lane Douglas': '',
+        'Lane Douglas': 'cab96d11-623a-4c21-92dd-8aad7ae3811a',
+        'Larry Hesketh': '0cb80846-a571-44ec-8c5d-1b02c8e00f5a',
         'Linda Turner': '4c43c37c-7867-4015-a065-7bec60b46153',
-        'Marvin Bernstein': '',
+        'Marvin Bernstein': 'a970ee37-f8ca-4013-b537-6be4d065e6fd',
         'Max Schneyder': 'd230afdb-0f56-451a-bd74-1d0de5179b28',
         'Michael Jones': '275c7139-c7c3-45b4-96bb-2b49b65932bf',
-        'Oliver Shore': '',
-        'Oliver Wilson': '',
+        'Oliver Shore': '29727e8b-26a6-4363-a51b-a99b53a565bc',
+        'Oliver Wilson': '9e768645-9cae-47ff-8ed9-edaf5ce777ef',
         'Otakar Olsanik': 'd205fff0-a176-40a5-98aa-b59b25ce084f',
-        'Peter Horner': '',
-        'Raoul Foster': '',
+        'Peter Horner': '1fe4b21f-1b95-4093-896a-8da438c62005',
+        'Raoul Foster': 'f06fb456-cabd-4231-9a07-90e170571449',
         'Wolf Silver': '88ff3394-6b06-4d1f-a48b-50e71b9fd193'
     };
