0764b7eca55478d26840a62c3d1feb34a6de40c0
brianfreud <brian.brianschweitzer@gmail.com>
2019-03-19 04:12:00 -0400

    Update utility_functions.js

--- utility_functions.js
+++ utility_functions.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 /* globals MBImport */
 // @name           Utility functions
-// @version        2019.3.18.7
+// @version        2019.3.19.0
 // @namespace      https://github.com/brianfreud
 // @downloadURL    https://raw.githubusercontent.com/brianfreud/Userscripts/edit/master/utility_functions.js
 // @updateURL      https://raw.githubusercontent.com/brianfreud/Userscripts/edit/master/utility_functions.js
