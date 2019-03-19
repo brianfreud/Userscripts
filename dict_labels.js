4c517514959050e66a2ab65a331a56c8d08de752
brianfreud <brian.brianschweitzer@gmail.com>
2019-03-18 23:40:31 -0400

    Update dict_labels.js
    Migrate codebase to a single exposed global variable.

--- dict_labels.js
+++ dict_labels.js
@@ -1,35 +1,110 @@
 // ==UserScript==
+/* globals         ß */
 // @name           Label dictionary
-// @version        2019.3.17.1
+// @version        2019.3.18.0
 // @namespace      https://github.com/brianfreud
 // @downloadURL    https://raw.githubusercontent.com/brianfreud/Userscripts/edit/master/dict_labels.js
 // @updateURL      https://raw.githubusercontent.com/brianfreud/Userscripts/edit/master/dict_labels.js
 // ==/UserScript==
 
-window.labelDB = {
-        'Arcadia Cinematic': '1c9fc74a-6645-4977-b143-e2d60464536e',
-        'Arcadia Classical Media Music': '',
-        'Arcadia Cosmos': '',
-        'Arcadia Cosmos Special Select': '',
-        'Arcadia DeLuxe': 'b7a82c40-36b7-445a-a5c3-c4f453193ff6',
-        'Arcadia Drones': '',
-        'Arcadia EnVogue': '',
-        'Arcadia Esovision': '',
-        'Arcadia Galaxy Music Lexicon': '',
-        'Arcadia Global Authentic Music': '',
-        'Arcadia Hollywood': '0251b61b-7545-4bc0-b487-975ef6db2857',
-        'Arcadia Jazz Line': '',
-        'Arcadia Jingle Bank': '',
-        'Arcadia Less-Is-More': '',
-        'Arcadia Lifestyle': '',
-        'Arcadia Loop Academy': '',
-        'Arcadia MasterTrax': '',
-        'Arcadia MaxMusic': '',
-        'Arcadia Media Music': 'de05ab86-1ef9-4dd9-8f82-206507365040',
-        'Arcadia Minimal': '',
-        'Arcadia National': '',
-        'Arcadia Shorts': '',
-        'Arcadia Spot Leitmotif': '',
-        'Arcadia Therapy': '',
-        'Arcadia Viva': ''
-    };
+ß.labelDB = [{
+        name: 'Arcadia Cinematic',
+        mbid: '1c9fc74a-6645-4977-b143-e2d60464536e'
+    },
+    {
+        name: 'Arcadia Classical Media Music',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia Cosmos',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia Cosmos Special Select',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia DeLuxe',
+        mbid: 'b7a82c40-36b7-445a-a5c3-c4f453193ff6'
+    },
+    {
+        name: 'Arcadia Drones',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia EnVogue',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia Esovision',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia Galaxy Music Lexicon',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia Global Authentic Music',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia Hollywood',
+        mbid: '0251b61b-7545-4bc0-b487-975ef6db2857'
+    },
+    {
+        name: 'Arcadia Jazz Line',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia Jingle Bank',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia Less-Is-More',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia Lifestyle',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia Loop Academy',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia MasterTrax',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia MaxMusic',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia Media Music',
+        mbid: 'de05ab86-1ef9-4dd9-8f82-206507365040'
+    },
+    {
+        name: 'Arcadia Minimal',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia National',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia Shorts',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia Spot Leitmotif',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia Therapy',
+        mbid: ''
+    },
+    {
+        name: 'Arcadia Viva',
+        mbid: ''
+    }
+];
