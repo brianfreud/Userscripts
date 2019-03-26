6e1c80319857b8146579b5d1fdb842186c6d0f95
brianfreud <brian.brianschweitzer@gmail.com>
2019-03-25 21:13:31 -0400

    Update ShowLogoOnLabelPage.user.js

--- ShowLogoOnLabelPage.user.js
+++ ShowLogoOnLabelPage.user.js
@@ -3,7 +3,7 @@
 // @author         Brian Schweitzer
 // @name           Show logo image on label pages at MusicBrainz
 // @description    Show logo image on label pages at MusicBrainz, if the AR exists
-// @version        2019.3.23.2
+// @version        2019.3.25.0
 // @namespace      https://github.com/brianfreud
 // @downloadURL    https://raw.githubusercontent.com/brianfreud/Userscripts/master/ShowLogoOnLabelPage.user.js
 // @updateURL      https://raw.githubusercontent.com/brianfreud/Userscripts/master/ShowLogoOnLabelPage.user.js
@@ -17,7 +17,7 @@
     $.getJSON(`${url[0]}//musicbrainz.org/ws/2/label/${url[4]}?inc=url-rels&fmt=json`, (data) => {
         let ar = data.relations.filter(ar => ar.type == 'logo');
         if (0 < ar.length) {
-            $('.commons-image').append(`<img src="${ar[0].url.resource}"/>`);
+            $('.commons-image').append(`<img style="max-width: 215px;" src="${ar[0].url.resource}"/>`);
         }
     });
 }
