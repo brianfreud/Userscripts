d9836dc5d1aa4391d0cdc01454f329905938b7bf
brianfreud <brian.brianschweitzer@gmail.com>
2019-03-26 10:23:37 -0400

    Update ExtremeMusic_importer.user.js

--- ExtremeMusic_importer.user.js
+++ ExtremeMusic_importer.user.js
@@ -2,7 +2,7 @@
 /* globals         MBImport, $, ß */
 // @name           Import ExtremeMusic release listings to MusicBrainz
 // @description    Add a button to import ExtremeMusic release listings to MusicBrainz
-// @version        2019.3.24.1
+// @version        2019.3.24.3
 // @namespace      https://github.com/brianfreud
 // @downloadURL    https://raw.githubusercontent.com/brianfreud/Userscripts/master/ExtremeMusic_importer.user.js
 // @updateURL      https://raw.githubusercontent.com/brianfreud/Userscripts/master/ExtremeMusic_importer.user.js
@@ -17,19 +17,13 @@
 // ==/UserScript==
 
 $.get(document.location.href, function(data) {
-    let i = 1;
+    let i = 0;
     const info = JSON.parse(data.match(/{\\"album\\".+</)[0].slice(0, -2).replace(/\\"/g, '"')),
-        date = info.album.created.match(/(\d{4})\-(\d\d)\-(\d\d)/),
-        times = $('.tracklist__track-item__cell.is--duration').map(function() {
-            return $(this).text().trim();
-        }),
-        splitArtist = (artist) => {
-            return artist.split(/\s[\-\/]\s/);
-        };
+        date = info.album.created.match(/(\d{4})\-(\d\d)\-(\d\d)/);
 
     Object.assign(ß.data, {
         artistList: new Set(),
-        catNum: info.album.abum_no,
+        catNum: info.album.album_no,
         label: info.album.series_title,
         releaseArtist: ["various_artists"],
         releaseName: ß.toTitleCase(info.album.title),
@@ -41,14 +35,20 @@ $.get(document.location.href, function(data) {
         totalTracks: 0
     });
 
-    info.tracks.forEach((track) => {
-        ß.data.tracks[i] = ['',
-            track.track_no.split('_')[1], // Track number
-            ß.toTitleCase(track.title),
-            track.composers.map(composer => composer.name), // Track artists
-            times[i]
-        ];
-        ß.data.artistList.add(ß.data.tracks[i++][3].join('&'));
+    info.tracks.forEach(track => {
+        track.track_sound_ids.forEach(id => {
+            let sound = info.track_sounds.filter(sound => sound.id === id)[0],
+                noAltVer = !(track.track_sound_ids.length - 1),
+                trackTitle = noAltVer ? track.title : `${sound.title} (${sound.version_type})`;
+            ß.data.tracks[++i] = [
+                track.track_sound_ids,
+                sound.track_sound_no.split('_').slice(1).join('.'), // Track number
+                ß.toTitleCase(trackTitle), // Track title
+                track.composers.map(composer => composer.name), // Track artists
+                ß.formatSeconds(sound.duration)
+            ];
+        });
+        ß.data.artistList.add(ß.data.tracks[i][3].join('&'));
     });
 
     if (ß.data.artistList.size === 1) { // If only one artist for release's tracks:
@@ -58,7 +58,6 @@ $.get(document.location.href, function(data) {
     const releaseObj = ß.buildReleaseObject('Digital Media'),
         edit_note = MBImport.makeEditNote(ß.data.url, 'ExtremeMusic', '', 'https://github.com/brianfreud/Userscripts/'),
         parameters = MBImport.buildFormParameters(releaseObj, edit_note),
-        mbButton = `<div style="background: lightgrey;border-radius: 4px;cursor: pointer;margin-top: 1em;padding: 4px;width: 9em;">${MBImport.buildFormHTML(parameters)}</div>`;
-
+        mbButton = `<div style="background:lightgrey;border-radius:4px;cursor:pointer;margin-top:1em;padding:4px;width:8em;">${MBImport.buildFormHTML(parameters)}</div>`;
     $('.tracklist-header__info:first').append($(mbButton));
 });
