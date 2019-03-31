3195f1794c794a1b0bbb9aa391f493f147765d79
brianfreud <brian.brianschweitzer@gmail.com>
2019-03-31 08:27:36 -0400

    Update MB_ScriptIdentifier.user.js

--- MB_ScriptIdentifier.user.js
+++ MB_ScriptIdentifier.user.js
@@ -3,7 +3,7 @@
 /* jshint          expr: true */
 // @name           MusicBrainz: Script identifier
 // @description    Identify the script of tracklist characters.  Guess/set the script on any import which uses murdos' importer code.
-// @version        2019.3.31.0
+// @version        2019.3.31.1
 // @namespace      https://github.com/brianfreud
 // @downloadURL    https://raw.githubusercontent.com/brianfreud/Userscripts/master/MB_ScriptIdentifier.user.js
 // @updateURL      https://raw.githubusercontent.com/brianfreud/Userscripts/master/MB_ScriptIdentifier.user.js
@@ -11,6 +11,7 @@
 // @include        *
 // @require        https://raw.githubusercontent.com/brianfreud/Userscripts/master/dict_scripts.js
 // ==/UserScript==
+
 ß.getTextImporter = () => {
     'use strict';
     let rawtext = [];
@@ -25,6 +26,15 @@
     var text = [];
     $('.even, .odd').each(function() {
         text.push($(this).find('td:eq(1)').text().trim());
+    });
+    return text.join('');
+};
+
+ß.getTextEditRelease = () => {
+    'use strict';
+    var text = [];
+    $('.track-name').each(function() {
+        text.push($(this).val().trim());
     });
     return text.join('');
 };
@@ -52,9 +62,9 @@
 ß.startMB = () => {
     'use strict';
     let template = `
-<div id="script-id-info" style="border: 2px dashed mediumslateblue;padding: 6px;border-radius: 5px;margin: 5px -6px;">
-    <table style="width:90%;font-size:110%;">
-        <caption style="text-align:left;padding-left:1em;white-space:nowrap;"><h2>Script(s) in use on release:</h2></caption>
+<aside id="script-id-info">
+    <table>
+        <caption style="white-space:nowrap;"><h2>Script(s) in track titles:</h2></caption>
         <thead>
             <tr>
                 <th style="text-align:center;">Chars</th>
@@ -64,21 +74,44 @@
         <tbody id="script-id-info-list">
         </tbody>
     </table>
-</div>`;
+</aside>`;
 
     if (document.URL.split('/')[3] === 'release') {
+        const generateScriptList = (list) => {
+            list = ß.matchScripts(list);
+            let scriptList = list.sort((a, b) => b[1].length - a[1].length)
+                .map(a => `<tr><td style="text-align:center;">${a[1].length}</td><td>${ß.scripts.filter(x => x.code == a[0])[0].name}</td></tr>`);
+            $('#script-id-info-list').empty().append(scriptList);
+        };
+
         if (document.URL.match(/(.+\/edit)|(\/add)/)) { // release editor page
-            $('.half-width:first').prepend($(template).css({ right: 0, position: 'absolute' }));
+            $('.half-width:first').prepend($(template).css({
+                border: '2px dashed mediumslateblue',
+                padding: '0 1em 2em 1em',
+                borderRadius: '5px',
+                margin: '5px -6px',
+                right: 0,
+                position: 'absolute'
+            }));
+
+            $('.advanced-disc').on('change', '.track-name', () => {
+                generateScriptList(ß.getTextEditRelease());
+            });
+
+            if (document.URL.match(/(.+\/edit)/)) {
+                let runCount = () => {
+                        if ($('.track-name').length > 0) {
+                            generateScriptList(ß.getTextEditRelease());
+                            clearInterval(checker);
+                        }
+                    },
+                    checker = setInterval(runCount, 100);
+            }
         } else if (document.URL.split('/').length === 5) { // main release page
             $('.properties:eq(1)').after(template);
-            const list = ß.matchScripts(ß.getTextViewRelease());
-            let scriptList = list.sort((a, b) => b[1].length - a[1].length)
-                .map(a => `<tr><td style="text-align:center;">>${a[1].length}</td><td>${a[0]}</td></tr>`);
-            $('#script-id-info-list').append(scriptList);
+            generateScriptList(ß.getTextViewRelease());
         }
     }
 };
 
-{
-    (document.URL.split('/')[2] === 'musicbrainz.org') ? ß.startMB: ß.startNonMB;
-}
+document.URL.split('/')[2] === 'musicbrainz.org' ? ß.startMB() : ß.startNonMB();
