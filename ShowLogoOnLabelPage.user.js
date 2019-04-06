16dd8c27418916735dac3ca635be7a13b2c56237
brianfreud <brian.brianschweitzer@gmail.com>
2019-04-06 11:06:39 -0400

    Update ShowLogoOnLabelPage.user.js
    Add ESLinting, solve the issue of all-white logos not being visible against the background

--- ShowLogoOnLabelPage.user.js
+++ ShowLogoOnLabelPage.user.js
@@ -1,23 +1,75 @@
 // ==UserScript==
-/* globals         $ */
 // @author         Brian Schweitzer
 // @name           Show logo image on label pages at MusicBrainz
 // @description    Show logo image on label pages at MusicBrainz, if the AR exists
-// @version        2019.3.25.0
+// @version        2019.4.6.1
 // @namespace      https://github.com/brianfreud
 // @downloadURL    https://raw.githubusercontent.com/brianfreud/Userscripts/master/ShowLogoOnLabelPage.user.js
 // @updateURL      https://raw.githubusercontent.com/brianfreud/Userscripts/master/ShowLogoOnLabelPage.user.js
 // @include        http*://musicbrainz.org/label/*
 // @require        https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
-
+// @require        https://raw.githubusercontent.com/brianfreud/Userscripts/master/getAverageRGB.js
+/* global          $, getAverageRGB */
+/* eslint          camelcase: off */
+/* eslint          capitalized-comments: off */
+/* eslint          dot-location: ["error", "property"] */
+/* eslint-env      es6, jquery */
+/* eslint          id-length: off */
+/* eslint          line-comment-position: off */
+/* eslint          max-len: off */
+/* eslint          multiline-comment-style: off */
+/* eslint          newline-per-chained-call: off */
+/* eslint          no-inline-comments: off */
+/* eslint          no-invalid-this: off */
+/* eslint          no-magic-numbers: off */
+/* eslint          no-whitespace-before-property: off */
+/* eslint          padded-blocks: off */
+/* eslint          prefer-destructuring: off */
+/* eslint          prefer-named-capture-group: off */
+/* eslint          quote-props: ["error", "as-needed"] */
+/* eslint          quotes: ["error", "backtick"] */
+/* eslint          sort-keys: off */
+/* eslint          sort-vars: off */
+/* eslint          spaced-comment: off */
+/* eslint          yoda: off */
 // ==/UserScript==
 
 {
-    let url = document.URL.split('/');
+
+    const url = document.URL.split(`/`);
+
     $.getJSON(`${url[0]}//musicbrainz.org/ws/2/label/${url[4]}?inc=url-rels&fmt=json`, (data) => {
-        let ar = data.relations.filter(ar => ar.type == 'logo');
+        'use strict';
+
+        const ar = data.relations.filter((rel) => rel.type === `logo`);
+
         if (0 < ar.length) {
-            $('.commons-image').append(`<img style="max-width: 215px;" src="${ar[0].url.resource}"/>`);
+
+            const imageURL = ar[0].url.resource,
+                img = new Image(),
+                src = `https://cors-anywhere.herokuapp.com/${imageURL}`;
+
+            $(`.commons-image`).append(`<img style="max-width: 215px;" src="${imageURL}" class="LabelLogo"/>`);
+
+            // Solve the issue of all-white logos not being visible against the background
+
+            img.onload = function onload () {
+
+                const RGB = getAverageRGB(img);
+
+                if (75 < RGB.r && 75 < RGB.g && 75 < RGB.b) {
+
+                    $(`.LabelLogo`).css(`filter`, `invert(100%)`);
+
+                }
+
+            };
+
+            img.crossOrigin = ``;
+            img.src = src;
+
         }
+
     });
+
 }
