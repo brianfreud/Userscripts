324ef6c4f520f292fbb24e4ba59bbf8693a4a419
brianfreud <brian.brianschweitzer@gmail.com>
2019-04-19 07:25:35 -0400

    Update e.js

--- e.js
+++ e.js
@@ -1,147 +1,203 @@
 // ==UserScript==
-// @name           Import Musou release listings to MusicBrainz TEST
-// @description    Add a button to import Musou release listings to MusicBrainz
-// @version        2019.4.15.0
-// @include        http://www.musou.gr/music/album/*
+/* globals MBImport, $ */
+// @name           Utility functions TEST
+// @version        2019.4.16.0
 // @namespace      https://github.com/brianfreud
-/* global          MBImport, ß, $ */
+// @downloadURL    https://raw.githubusercontent.com/brianfreud/Userscripts/edit/master/utility_functions.js
+// @updateURL      https://raw.githubusercontent.com/brianfreud/Userscripts/edit/master/utility_functions.js
+/* eslint          array-bracket-newline: off */
+/* eslint          array-element-newline: off */
+/* eslint          brace-style: ["error", "stroustrup", { "allowSingleLine": true }] */
 /* eslint          camelcase: off */
 /* eslint          capitalized-comments: off */
 /* eslint          dot-location: ["error", "property"] */
-/* eslint-env      es6, jquery */
+/* eslint-env      es6, $ */
 /* eslint          id-length: off */
+/* eslint          key-spacing: off */
 /* eslint          line-comment-position: off */
 /* eslint          max-len: off */
 /* eslint          multiline-comment-style: off */
 /* eslint          newline-per-chained-call: off */
+/* eslint          no-extra-parens: ["error", "all", { "nestedBinaryExpressions": false }] */
 /* eslint          no-inline-comments: off */
 /* eslint          no-invalid-this: off */
-/* eslint          no-sparse-arrays: off */
 /* eslint          no-magic-numbers: off */
+/* eslint          no-param-reassign: off */
+/* eslint          no-plusplus: off */
+/* eslint          no-ternary: off */
+/* eslint          no-whitespace-before-property: off */
 /* eslint          one-var: off */
 /* eslint          padded-blocks: off */
 /* eslint          prefer-destructuring: off */
 /* eslint          prefer-named-capture-group: off */
+/* eslint          prefer-reflect: off */
 /* eslint          quote-props: ["error", "as-needed"] */
 /* eslint          quotes: ["error", "backtick"] */
 /* eslint          sort-keys: off */
 /* eslint          sort-vars: off */
 /* eslint          spaced-comment: off */
-// @downloadURL    https://raw.githubusercontent.com/brianfreud/Userscripts/master/Musou_importer.user.js
-// @updateURL      https://raw.githubusercontent.com/brianfreud/Userscripts/master/Musou_importer.user.js
-// @require        https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
-// @require        https://raw.githubusercontent.com/brianfreud/Userscripts/master/e.js
-// @require        https://raw.githubusercontent.com/brianfreud/Userscripts/master/dict_artists.js
-// @require        https://raw.githubusercontent.com/brianfreud/Userscripts/master/dict_labels.js
-// @require        https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/master/lib/mbimport.js
-// @require        https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/master/lib/mbimportstyle.js
-// @icon           https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/master/assets/images/Musicbrainz_import_logo.png
 // ==/UserScript==
+
+const ß = {};
 
 (function strictWrapper () {
     'use strict';
 
-    const getTrackComposer = (el) => {
-        const infoDTs = el.closest(`.track-line`)
-            .querySelectorAll(`.expand-row dt`);
+    Object.defineProperties(String.prototype, { // eslint-disable-line no-extend-native
+        /**
+         * Removes a string or regex match from a string.
+         * @extends external:String
+         * @param {string|regex} matcher - The match expression to remove
+         */
+        remove: {
+            value: (matcher) => this.replace(matcher, ``)
+        },
 
-        return [...infoDTs].filter((dt) => dt.textContent === `Composers`)[0];
-    };
-
-    const scrapeMainTracks = () => {
-        ß.data.tracks = ß.getTracks({
-            trackSelector: `.parent .track-title`,
-            parseTrack: (el) => [
-                ``, // key
-                el.textContent.toLowerCase().split(`. `), // [number, title]
-                getTrackComposer(el).nextElementSibling.textContent, // artist
-                el.nextSibling.textContent // duration
-            ].flat()
-        });
-    };
-
-    const scrapeAltTracks = () => {
-        ß.data.tracks.push(ß.getTracks({
-            trackSelector: `.other-versions .track-title`,
-            parseTrack: (el) => [
-                ``, // key
-                `${el.parentNode.textContent} (${parent.querySelector(`.comment`).textContent})`.toLowerCase().split(`. `), // [number, title]
-                getTrackComposer(el).nextElementSibling.textContent, // artist
-                el.parentNode.querySelector(`.duration`).textContent.remove(/^\s*00:/u) // duration
-            ].flat()
-        }));
-    };
-
-
-    // ------------------------------------------------
-
-
-    ß.parseTracks = () => {
-        ß.data.tracks = ß.data.tracks.map((track) => [
-            track[0].split(/^(\d+)\.\s/u),
-            track[1],
-            track[2]
-        ]).map((track) => [
-            ``,
-            track[0][1],
-            track[0][2].toLowerCase(),
-            track[2].replace(/\d+%/gu, ``)
-                .replace(/\([A-Z]+\)/gu, ``)
-                .replace(/\//gu, `, `)
-                .replace(/\[\d+\]/u, ``)
-                .split(`,`)
-                .map((artist) => {
-                    ß.data.artistList.add(track[2]);
-
-                    return artist.trim();
-                }),
-            track[1].match(/\d\d:\d\d$/u)[0]
-        ]).sort((first, second) => first[1] - second[1]);
-
-        ß.data.totalTracks = ß.data.tracks.length;
-    };
-
-    ß.scrapeRelease = () => {
-        const dds = $(`.track-line:first .expand-row dt`).filter(`:contains(Sub-label), :contains(Album), :contains(Album code), :contains(Year)`)
-            .map(function getText () {
-                return this.nextElementSibling.innerText;
-            });
-
-        Object.assign(ß.data, {
-            catNum: dds[2],
-            label: dds[0].toLowerCase(),
-            releaseArtist: [`various_artists`],
-            releaseName: dds[1].replace(` Vol. `, `, Volume `),
-            url: document.location.href,
-            year: dds[3]
-        });
-    };
-
-    ß.checkReleaseArtist = () => {
-        if (ß.data.artistList.size === 1) { // If only one artist for release's tracks:
-            ß.data.releaseArtist = ß.data.tracks[1][3];
+        /**
+         * Converts a string to titlecase.
+         * @extends external:String
+         */
+        toTitleCase: {
+            value: () => this.toLowerCase()
+                .split(` `)
+                .map((a) => a.substr(0, 1).toUpperCase() + a.substr(1))
+                .join(` `)
         }
-    };
+    });
 
-    ß.makeImportButton = () => {
-        const edit_note = MBImport.makeEditNote(ß.data.url, `Mousou`, ``, `https://github.com/brianfreud/Userscripts/`),
-            releaseObj = ß.buildReleaseObject(`Digital Media`),
-            parameters = MBImport.buildFormParameters(releaseObj, edit_note),
-            mbButton = `<td style="width: 28%;text-align:right;color:black;">${MBImport.buildFormHTML(parameters)}</td>`;
+    Object.assign(ß, {
+        data: {},
 
-        $(`.description:first`).after(mbButton);
-    };
+        getTracks: (obj) => {
+            const allTracks = Object.freeze(document.querySelectorAll(obj.trackSelector).entries()),
+                trackDB = [];
 
-    ß.data.artistList = new Set();
-    ß.data.tracks = [];
-    scrapeMainTracks();
-    scrapeAltTracks();
-    ß.setTotalTracks();
-//    ß.parseTracks();
-    ß.scrapeRelease();
-console.dir(ß.data);    
-    ß.checkReleaseArtist();
-    ß.makeImportButton();
-    // ------------------------------------------------
+            for (const [, element] of allTracks) {
+                trackDB.push(obj.parseTrack(element));
+            }
 
+            return trackDB;
+        },
+
+// ---------------------------------------------------
+
+        buildArtistCredit: (names) => {
+            const creditArr = [];
+
+            for (const [i, name] of [...names].entries()) {
+                if (name === `various_artists` || name === `unknown`) {
+                    creditArr.push(MBImport.specialArtist(name));
+                }
+                else {
+                    const creditObj = {
+                        artist_name: name.toTitleCase(),
+                        mbid: ß.artistDB[name.toLowerCase()] || ``
+                    };
+
+                    if (names.length > 1) {
+                        if (names.length - 1 !== i) {
+                            creditObj.joinphrase = names.length - 2 === i
+                                ? ` & `
+                                : `, `;
+                        }
+                    }
+                    creditArr.push(creditObj);
+                }
+            }
+
+            return creditArr;
+        },
+
+        buildLabelCredit: () => {
+            const label = ß.labelDB.filter((p) => p.name === ß.data.label.toLowerCase()); // Find any label's specific object, if it is in the labelDB array
+
+            if (typeof label === `undefined` || !label.length) {
+                return [{
+                    catno: ß.data.catNum,
+                    name: ß.data.label.toTitleCase()
+                }];
+            } // else
+
+            return [{
+                catno: ß.data.catNum,
+                country: `country` in label[0]
+                    ? label[0].country
+                    : ``,
+                mbid: label[0].mbid,
+                name: label[0].name
+            }];
+        },
+
+        buildTracklistArray: () => {
+            const trackArray = [];
+
+            ß.data.tracks = [...new Set(ß.data.tracks)];
+
+            for (const track of ß.data.tracks) {
+                if (typeof track !== `undefined`) {
+                    trackArray.push({
+                        number: track[1],
+                        title: track[2],
+                        duration: track[4],
+                        artist_credit: ß.buildArtistCredit(track[3])
+                    });
+                }
+            }
+
+            return trackArray;
+        },
+
+        buildReleaseObject: (format = `CD`) => {
+            const releaseObj = {
+                title: ß.data.releaseName.replace(` - Volume`, `, Volume`),
+                artist_credit: ß.buildArtistCredit(ß.data.releaseArtist),
+                type: `album`,
+                status: `official`,
+                language: `eng`,
+                script: `Latn`,
+                year: ß.data.year || ``,
+                month: ß.data.month || ``,
+                day: ß.data.day || ``,
+                labels: ß.buildLabelCredit(),
+                urls: [{
+                    url: ß.data.url,
+                    link_type: 288
+                }],
+                discs: [{
+                    format,
+                    tracks: ß.buildTracklistArray()
+                }]
+            };
+
+            releaseObj.country = releaseObj.labels[0].country;
+
+            return releaseObj;
+        },
+
+        formatSeconds: (seconds) => ((seconds - (seconds %= 60)) / 60) + (seconds > 9
+            ? `:`
+            : `:0`) + seconds,
+
+        unSortname: (str) => { // Turns "Jones, Bob" back into "Bob Jones"
+            const name = str.split(`,`)
+                .map((a) => a.trim());
+
+            return [name.splice(-1), name].flat().join(` `);
+        },
+
+        // Turn ["Jones, Bob"] into ["bob jones"]
+        unSortnameArray: (arr) => arr.map((name) => ß.unSortname(name.toLowerCase())),
+
+        getIDText: (str) => ß.$getID(str).text(),
+
+        $getTDs: (node) => $(node).find(`td`),
+
+        getTDText: ($nodes, i) => $.trim($nodes.eq(i).text()),
+
+        buildImportTools: (prefix = ``) => {
+            Object.assign(ß, {
+                $getID: (str) => $(`#${prefix}${str}`)
+            });
+        }
+    });
 }());
