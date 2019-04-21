3e4cc74d286cfdcae0d4701acffb63522001a96c
brianfreud <brian.brianschweitzer@gmail.com>
2019-04-21 05:53:09 -0400

    Update ExtremeMusic_importer.user.js

--- ExtremeMusic_importer.user.js
+++ ExtremeMusic_importer.user.js
@@ -1,13 +1,45 @@
 // ==UserScript==
-/* globals         MBImport, $, ß */
-// @name           Import ExtremeMusic release listings to MusicBrainz
-// @description    Add a button to import ExtremeMusic release listings to MusicBrainz
-// @version        2019.3.24.3
+// @name           Import Extreme Music release listings to MusicBrainz
+// @description    Add a button to import Extreme Music release listings to MusicBrainz
+// @version        2019.4.20.2
+// @include        https://www.extrememusic.com/albums/*
 // @namespace      https://github.com/brianfreud
+/* global          MBImport, ß */
+/* eslint          array-bracket-newline: off */
+/* eslint          array-element-newline: off */
+/* eslint          brace-style: ["error", "stroustrup", { "allowSingleLine": true }] */
+/* eslint          camelcase: off */
+/* eslint          capitalized-comments: off */
+/* eslint          dot-location: ["error", "property"] */
+/* eslint-env      es6, jquery */
+/* eslint          id-length: off */
+/* eslint          key-spacing: off */
+/* eslint          line-comment-position: off */
+/* eslint          max-len: off */
+/* eslint          max-lines: off */
+/* eslint          multiline-comment-style: off */
+/* eslint          newline-per-chained-call: off */
+/* eslint          no-extra-parens: ["error", "all", { "nestedBinaryExpressions": false }] */
+/* eslint          no-inline-comments: off */
+/* eslint          no-invalid-this: off */
+/* eslint          no-magic-numbers: off */
+/* eslint          no-param-reassign: off */
+/* eslint          no-plusplus: off */
+/* eslint          no-ternary: off */
+/* eslint          no-whitespace-before-property: off */
+/* eslint          object-curly-spacing: off */
+/* eslint          one-var: off */
+/* eslint          padded-blocks: off */
+/* eslint          prefer-destructuring: off */
+/* eslint          prefer-named-capture-group: off */
+/* eslint          prefer-reflect: off */
+/* eslint          quote-props: ["error", "as-needed"] */
+/* eslint          quotes: ["error", "backtick"] */
+/* eslint          sort-keys: off */
+/* eslint          sort-vars: off */
+/* eslint          spaced-comment: off */
 // @downloadURL    https://raw.githubusercontent.com/brianfreud/Userscripts/master/ExtremeMusic_importer.user.js
 // @updateURL      https://raw.githubusercontent.com/brianfreud/Userscripts/master/ExtremeMusic_importer.user.js
-// @include        https://www.extrememusic.com/albums/*
-// @require        https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require        https://raw.githubusercontent.com/brianfreud/Userscripts/master/utility_functions.js
 // @require        https://raw.githubusercontent.com/brianfreud/Userscripts/master/dict_artists.js
 // @require        https://raw.githubusercontent.com/brianfreud/Userscripts/master/dict_labels.js
@@ -16,48 +48,75 @@
 // @icon           https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/master/assets/images/Musicbrainz_import_logo.png
 // ==/UserScript==
 
-$.get(document.location.href, function(data) {
-    let i = 0;
-    const info = JSON.parse(data.match(/{\\"album\\".+</)[0].slice(0, -2).replace(/\\"/g, '"')),
-        date = info.album.created.match(/(\d{4})\-(\d\d)\-(\d\d)/);
+(function strictWrapper () { // eslint-disable-line max-statements
 
-    Object.assign(ß.data, {
-        artistList: new Set(),
-        catNum: info.album.album_no,
-        label: info.album.series_title,
-        releaseArtist: ["various_artists"],
-        releaseName: ß.toTitleCase(info.album.title),
-        year: date[1],
-        month: date[2],
-        day: date[3],
-        tracks: [],
-        url: document.location.href,
-        totalTracks: 0
-    });
+    'use strict';
 
-    info.tracks.forEach(track => {
-        track.track_sound_ids.forEach(id => {
-            let sound = info.track_sounds.filter(sound => sound.id === id)[0],
-                noAltVer = !(track.track_sound_ids.length - 1),
-                trackTitle = noAltVer ? track.title : `${sound.title} (${sound.version_type})`;
-            ß.data.tracks[++i] = [
-                track.track_sound_ids,
-                sound.track_sound_no.split('_').slice(1).join('.'), // Track number
-                ß.toTitleCase(trackTitle), // Track title
-                track.composers.map(composer => composer.name), // Track artists
-                ß.formatSeconds(sound.duration)
-            ];
+    const scrapeTracks = (info) => {
+        info.tracks.forEach((track) => {
+            track.track_sound_ids.forEach((id) => {
+                const sound = info.track_sounds.filter((thisSound) => thisSound.id === id)[0],
+                    noAltVer = !(track.track_sound_ids.length - 1),
+                    trackTitle = noAltVer
+                        ? track.title
+                        : `${sound.title} (${sound.version_type})`;
+
+                ß.data.tracks.push({
+                    number: sound.track_sound_no.split(`_`).slice(1).join(`.`).remove(/^0+/u),
+                    title: trackTitle.toTitleCase(),
+                    artist: track.composers.map((composer) => composer.name),
+                    duration: ß.formatSeconds(sound.duration)
+                });
+            });
         });
-        ß.data.artistList.add(ß.data.tracks[i][3].join('&'));
-    });
+    };
 
-    if (ß.data.artistList.size === 1) { // If only one artist for release's tracks:
-        ß.data.releaseArtist = ß.data.tracks[1][3];
-    }
+    const getReleaseInfo = (info) => {
+        const date = info.album.created.match(/(\d{4})-(\d\d)-(\d\d)/u);
 
-    const releaseObj = ß.buildReleaseObject('Digital Media'),
-        edit_note = MBImport.makeEditNote(ß.data.url, 'ExtremeMusic', '', 'https://github.com/brianfreud/Userscripts/'),
-        parameters = MBImport.buildFormParameters(releaseObj, edit_note),
-        mbButton = `<div style="background:lightgrey;border-radius:4px;cursor:pointer;margin-top:1em;padding:4px;width:8em;">${MBImport.buildFormHTML(parameters)}</div>`;
-    $('.tracklist-header__info:first').append($(mbButton));
-});
+        Object.assign(ß.data, {
+            catNum: info.album.album_no,
+            label: info.album.series_title,
+            releaseName: info.album.title.toTitleCase(),
+            year: date[1],
+            month: date[2],
+            day: date[3],
+            url: document.location.href
+        });
+    };
+
+    const makeImportButton = function makeImportButton () {
+        const parameters = ß.buildImportButton({
+                site: `Extreme Music`
+            }),
+            button = document.createElement(`div`);
+
+        Object.assign(button.style, {
+            backgroundColor: `lightgrey`,
+            borderRadius:    `4px`,
+            cursor:          `pointer`,
+            marginTop:       `1em`,
+            padding:         `4px`,
+            width:           `8em`
+        });
+        button.innerHTML = MBImport.buildFormHTML(parameters);
+        document.querySelectorAll(`.tracklist-header__info`)[0].appendChild(button);
+    };
+
+    (async function processData () {
+        const info = await fetch(document.location.href)
+            .then((response) => response.text())
+            .then((html) => html.match(/\{\\"album\\".+</u)[0].slice(0, -2).replace(/\\"/ug, `"`)) // extract the JSON from the page's HTML
+            .then((json) => JSON.parse(json));
+
+        getReleaseInfo(info);
+        ß.data.tracks = [];
+        scrapeTracks(info);
+        ß.sortTracks();
+        ß.setTotalTracks();
+        ß.cleanTrackArtists();
+        ß.lookupTrackArtists();
+        ß.setReleaseArtist();
+        makeImportButton();
+    }());
+}());
