12ea1c6a1e009452e996bb2b5db88a0c587b112d
brianfreud <brian.brianschweitzer@gmail.com>
2019-04-21 05:52:27 -0400

    Update Musou_importer.user.js

--- Musou_importer.user.js
+++ Musou_importer.user.js
@@ -1,31 +1,40 @@
 // ==UserScript==
-// @name           Import Musou release listings to MusicBrainz
+// @name           Import Musou release listings to MusicBrainz TEST
 // @description    Add a button to import Musou release listings to MusicBrainz
-// @version        2019.4.20.0
+// @version        2019.4.21.0
 // @include        http://www.musou.gr/music/album/*
 // @namespace      https://github.com/brianfreud
-/* global          MBImport, ß, $ */
+/* global          MBImport, ß */
 /* eslint          array-bracket-newline: off */
 /* eslint          array-element-newline: off */
+/* eslint          brace-style: ["error", "stroustrup", { "allowSingleLine": true }] */
 /* eslint          camelcase: off */
 /* eslint          capitalized-comments: off */
 /* eslint          dot-location: ["error", "property"] */
-/* eslint-env      es6, jquery */
+/* eslint-env      es6 */
 /* eslint          id-length: off */
+/* eslint          key-spacing: off */
 /* eslint          line-comment-position: off */
-/* eslint          lines-around-directive: off */
 /* eslint          max-len: off */
+/* eslint          max-lines: off */
 /* eslint          multiline-comment-style: off */
 /* eslint          newline-per-chained-call: off */
+/* eslint          no-confusing-arrow: off */
+/* eslint          no-extra-parens: ["error", "all", { "nestedBinaryExpressions": false }] */
 /* eslint          no-inline-comments: off */
 /* eslint          no-invalid-this: off */
-/* eslint          no-sparse-arrays: off */
 /* eslint          no-magic-numbers: off */
-/* eslint          object-curly-spacing: */
+/* eslint          no-param-reassign: off */
+/* eslint          no-plusplus: off */
+/* eslint          no-return-assign: "error" */
+/* eslint          no-ternary: off */
+/* eslint          no-whitespace-before-property: off */
+/* eslint          object-curly-spacing: off */
 /* eslint          one-var: off */
 /* eslint          padded-blocks: off */
 /* eslint          prefer-destructuring: off */
 /* eslint          prefer-named-capture-group: off */
+/* eslint          prefer-reflect: off */
 /* eslint          quote-props: ["error", "as-needed"] */
 /* eslint          quotes: ["error", "backtick"] */
 /* eslint          sort-keys: off */
@@ -33,7 +42,6 @@
 /* eslint          spaced-comment: off */
 // @downloadURL    https://raw.githubusercontent.com/brianfreud/Userscripts/master/Musou_importer.user.js
 // @updateURL      https://raw.githubusercontent.com/brianfreud/Userscripts/master/Musou_importer.user.js
-// @require        https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require        https://raw.githubusercontent.com/brianfreud/Userscripts/master/utility_functions.js
 // @require        https://raw.githubusercontent.com/brianfreud/Userscripts/master/dict_artists.js
 // @require        https://raw.githubusercontent.com/brianfreud/Userscripts/master/dict_labels.js
@@ -43,6 +51,7 @@
 // ==/UserScript==
 
 (function strictWrapper () { // eslint-disable-line max-statements
+
     'use strict';
 
     const getTrackComposer = (el) => {
@@ -77,29 +86,31 @@
     };
 
     const getReleaseInfo = () => {
-        const dds = $(`.track-line:first .expand-row dt`).filter(`:contains(Sub-label), :contains(Album), :contains(Album code), :contains(Year)`)
-            .map(function getText () {
-                return this.nextElementSibling.innerText;
-            });
+        const dlData = {},
+            dtList = document.querySelector(`.expand-row`).querySelectorAll(`dt`),
+            ifPropExists = (prop) => prop in dlData
+                ? dlData[prop]
+                : ``;
+
+        Array.from(dtList).map((dt) => (dlData[dt.textContent] = dt.nextElementSibling.textContent)); // eslint-disable-line no-extra-parens
 
         Object.assign(ß.data, {
-            catNum: dds[2],
-            label: dds[0].toLowerCase(),
-            releaseName: dds[1],
+            catNum: ifPropExists(`Album code`),
+            label: ifPropExists(`Sub-label`).toLowerCase(),
+            releaseName: ifPropExists(`Album`),
             url: document.location.href,
-            year: dds[3]
+            year: ifPropExists(`Year`).match(/\d{4}/u)[0] // avoid issues with data like "2019;2019"
         });
     };
 
     const makeImportButton = function makeImportButton () {
-        const parameters = ß.ggmakeImportButton({
-                site: `Musou`
-            }),
+        const parameters = ß.buildImportButton({ site: `Musou` }),
             mbButton = `<td style="width: 28%;text-align:right;color:black;">${MBImport.buildFormHTML(parameters)}</td>`;
 
-        $(`.description:first`).after(mbButton);
+        document.querySelector(`.description`).insertAdjacentHTML(`afterend`, mbButton);
     };
 
+    getReleaseInfo();
     ß.data.tracks = [];
     scrapeMainTracks();
     scrapeAltTracks();
@@ -108,6 +119,5 @@
     ß.cleanTrackArtists();
     ß.lookupTrackArtists();
     ß.setReleaseArtist();
-    getReleaseInfo();
     makeImportButton();
 }());
