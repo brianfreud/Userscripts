04e90654c38c4ef4f078a6d4e70cbbc409e04496
brianfreud <brian.brianschweitzer@gmail.com>
2019-05-05 13:26:51 -0400

    Update HarvestMediaSG2+3_importer.user.js

--- HarvestMediaSG2+3_importer.user.js
+++ HarvestMediaSG2+3_importer.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
-// @name           Import Harvest Media SG2/SG3 release listings to MusicBrainz
-// @description    Add a button to import Harvest Media (SG2 & SG3 servers) release listings to MusicBrainz
-// @version        2019.5.4.3
+// @name           Import Harvest Media SG2/SG3/LIVE release listings to MusicBrainz
+// @description    Add a button to import Harvest Media (LIVE, SG2 & SG3 servers) release listings to MusicBrainz
+// @version        2019.5.5.0
+// @include        http*://live.harvestmedia.net*
 // @include        http*://www.westonemusic.com*
 // @include        http*://echomusicpg.sg2.harvestmedia.net*
 // @include        http*://indiesonics.sg2.harvestmedia.net*
@@ -68,8 +69,19 @@
 
     'use strict';
 
+    const GET = `GET`,
+        POST = `POST`;
+
     const displayError = function displayError (errMsg) {
-        if (ß.data.MODE === `SG2`) {
+        // -----------------------------------------------------------------
+
+        if (ß.data.MODE === `LIVE`) {
+            // TO DO
+        }
+
+        // -----------------------------------------------------------------
+
+        else if (ß.data.MODE === `SG2`) {
             document.querySelector(`.albumTrackView_AlbumInfoCenter`)
                 .appendChild(ß.makeFragmentFromString(`<div id="importError" style="color:red;font-size:125%;font-weight:900;"><br>${errMsg}</div>`));
         }
@@ -82,15 +94,14 @@
 
         // -----------------------------------------------------------------
 
-        else {
-            displayError(`Unknown mode for site.`);
-            ß.data.errorState = 1;
-
-            return; // eslint-disable-line no-useless-return
-        }
+        else console.log(errMsg); // eslint-disable-line curly, no-console
 
         // -----------------------------------------------------------------
+    };
 
+    const setBadMode = () => {
+        displayError(`Unknown mode for site.`);
+        ß.data.errorState = 1;
     };
 
     const populateLabelDB = async function populateLabelDB () {
@@ -99,12 +110,32 @@
 
             // -----------------------------------------------------------------
 
-            if (ß.data.MODE === `SG2`) {
+            if (ß.data.MODE === `LIVE`) {
+                await fetch(`${document.location.origin}/content/library_list.aspx?${ß.makeArgString(ß.data.reqArgs)}`, {
+                    body: null,
+                    cache: `force-cache`,
+                    method: GET
+                })
+                    .then((txt) => txt.text())
+                    .then((txt) => ß.makeFragmentFromString(txt))
+                    .then((frag) => {
+                        const libraryNodes = frag.querySelectorAll(`.ListTextLibrary`);
+
+                        ß.data.LABELS = [...libraryNodes].map((node) => ({
+                            id: node.getAttribute(`id`).split(`_`).pop(),
+                            name: node.innerText
+                        }));
+                    });
+            }
+
+            // -----------------------------------------------------------------
+
+            else if (ß.data.MODE === `SG2`) {
 
                 await fetch(`${document.location.origin}//Handler/GetLeftMenuItems.ashx?type=library`, {
                     body: null,
                     cache: `force-cache`,
-                    method: `GET`
+                    method: GET
                 })
                     .then((txt) => txt.text())
                     .then((txt) => ß.makeFragmentFromString(txt))
@@ -134,7 +165,7 @@
                 else { // generic SG3 sites
                     await fetch(`${document.location.origin}/Handler/GetLeftMenuItems.ashx`, {
                         body: `{"type":"library"}`,
-                        method: `POST`
+                        method: POST
                     })
                         .then((response) => Object.freeze(response.json()))
                         .then((json) => {
@@ -150,12 +181,7 @@
 
             // -----------------------------------------------------------------
 
-            else {
-                displayError(`Unknown mode for site.`);
-                ß.data.errorState = 1;
-
-                return;
-            }
+            else setBadMode(); // eslint-disable-line curly
 
             // -----------------------------------------------------------------
 
@@ -171,16 +197,26 @@
             label: data.albumLibraryName || ß.data.LABELS.filter((labelEntry) => labelEntry.id === (data.LibraryIdEnc || data.LibraryId))[0].name,
             releaseName: ß.unentity(data.albumName || data.DisplayTitle.remove(`${data.AlbumCode} `) || data.CdTitle),
             tracks: []
-        }, ß.extractDMY(new Date(data.ReleaseDate || data.albumReleased)));
+        });
 
-        if (ß.data.MODE === `SG2`) {
+        // -----------------------------------------------------------------
+
+        if (ß.data.MODE === `LIVE`) {
+            ß.data.url = `${document.location.origin}/player.aspx?${ß.makeArgString(ß.data.reqArgs)}`;
+        }
+
+        // -----------------------------------------------------------------
+
+        else if (ß.data.MODE === `SG2`) {
             ß.data.url = `${document.location.origin}/album/${data.IdEnc}${document.location.search}`;
+            Object.assign(ß.data, ß.extractDMY(new Date(data.ReleaseDate || data.albumReleased)));
         }
 
         // -----------------------------------------------------------------
 
         else if (ß.data.MODE === `SG3`) {
             ß.data.url = document.location.href;
+            Object.assign(ß.data, ß.extractDMY(new Date(data.ReleaseDate || data.albumReleased)));
         }
 
         // -----------------------------------------------------------------
@@ -210,7 +246,25 @@
         ß.data.trackNumbers = new Set();
 
         for (const track of data) {
-            if (ß.data.MODE === `SG2`) {
+
+            // -----------------------------------------------------------------
+
+            if (ß.data.MODE === `LIVE`) {
+                const number = parseInt(track.tracknumber, 10),
+                    trackID = ß.data.MOREINFO_HTML.querySelector(`#album_contentid_${number - 1}`).value;
+
+                ß.data.tracks.push({
+                    artist: ß.unSortnameAmpString(ß.data.MOREINFO_HTML.querySelector(`#album_composer_${trackID}`).value),
+                    duration: ß.formatSeconds(track.duration),
+                    number,
+                    title: ß.unentity(track.title).toLowerCase()
+                });
+                ß.data.trackNumbers.add(number);
+            }
+
+            // -----------------------------------------------------------------
+
+            else if (ß.data.MODE === `SG2`) {
                 const titlePattern1 = /.*trk(?<number>\d+)\s(?<title>.+)/iu,
                     titlePattern2 = /^(?<number>\d+)\s(?:-\s)?(?<title>.+)/u,
                     splitTitle = track.title.split(titlePattern1),
@@ -225,7 +279,6 @@
                 }
                 catch (err) { // The track ID isn't in the artist info
                     ß.data.errorState = 1;
-                    console.error(err); // eslint-disable-line no-console
                     displayError(`The release listing has incomplete artist information.`);
 
                     return;
@@ -259,7 +312,7 @@
                             'content-type': `application/x-www-form-urlencoded; charset=UTF-8`
                         },
                         body: `objectid=9549110ccb9334c6&playlisttype=Playlist_AlbumTab`,
-                        method: `POST`
+                        method: POST
                     })
                         .then((txt) => txt.text())
                         .then((blob) => eval(blob)) // eslint-disable-line no-eval
@@ -272,12 +325,7 @@
 
             // -----------------------------------------------------------------
 
-            else {
-                displayError(`Unknown mode for site.`);
-                ß.data.errorState = 1;
-
-                return;
-            }
+            else setBadMode(); // eslint-disable-line curly
         }
 
         const highestTrackNumber = Math.max(...ß.data.trackNumbers);
@@ -297,7 +345,7 @@
     };
 
     const makeImportButton = function makeImportButton () {
-        const parameters = ß.buildImportButton({ site: document.title }),
+        const parameters = ß.buildImportButton({ site: `Harvest Media` }),
             mbButton = MBImport.buildFormHTML(parameters);
 
         // -----------------------------------------------------------------
@@ -306,7 +354,19 @@
 
         let mbBtnPos; // eslint-disable-line init-declarations
 
-        if (ß.data.MODE === `SG2`) {
+        // -----------------------------------------------------------------
+
+        if (ß.data.MODE === `LIVE`) {
+            if (document.querySelector(`#mbPos`) === null) {
+                document.querySelector(`#tblNavBar`).parentNode.parentNode.nextElementSibling.querySelector(`td`)
+                    .appendChild(ß.makeFragmentFromString(`<div id="mbPos" style="white-space: nowrap;">`));
+            }
+            mbBtnPos = document.querySelector(`#mbPos`);
+        }
+
+        // -----------------------------------------------------------------
+
+        else if (ß.data.MODE === `SG2`) {
             mbBtnPos = document.querySelector(`.albumTrackView_AlbumInfoRightTop`);
             mbBtnPos.setAttribute(`id`, `mbPos`);
         }
@@ -323,15 +383,9 @@
 
         // -----------------------------------------------------------------
 
-        else {
-            displayError(`Unknown mode for site.`);
-            ß.data.errorState = 1;
-
-            return;
-        }
+        else setBadMode(); // eslint-disable-line curly
 
         // -----------------------------------------------------------------
-
 
         const mbForm = ß.makeFragmentFromString(`<br><div id="importButton" >${mbButton}</div>`);
 
@@ -349,24 +403,33 @@
     const setListener = function setListener (albumMenuNode) {
         albumMenuNode.addEventListener(`click`, (e) => {
             let albumID, requestPromise, objectcode; // eslint-disable-line init-declarations
+
+            // <-- Clean up anything from any previous release that may have been loaded this session
             const mbBtnPos = document.querySelector(`#mbPos`);
 
             ß.deleteNode(`#importError`);
+            Object.prototype.hasOwnProperty.call(ß.data, `reqArgs`) && Object.prototype.hasOwnProperty.call(ß.data.reqArgs, `collectionid`) && (ß.data.reqArgs.collectionid = ``);
             mbBtnPos !== null && mbBtnPos.shadowRoot && (mbBtnPos.shadowRoot.innerHTML = ``); // Remove any pre-existing buttons
+            // --> Finished cleaning up
 
             if (e.target.tagName === `A`) {
                 albumID = e.target.getAttribute(`objectid`) || e.target.getAttribute(`objectname`); // generic SG2 sites
-                if (albumID === null) { // West One Music, (others?)
-                    const shareLink = e.target.parentNode.parentNode.querySelector(`.share-button`);
+                if (albumID === null) {
+                    if (ß.data.MODE === `LIVE`) { // LIVE
+                        ß.data.reqArgs.collectionid = albumID = e.target.getAttribute(`onclick`).match(/'(.+)'/u)[1]; // eslint-disable-line no-multi-assign
+                    }
+                    else { // West One Music, (others?)
+                        const shareLink = e.target.parentNode.parentNode.querySelector(`.share-button`);
 
-                    if (shareLink !== null) {
-                        albumID = shareLink.getAttribute(`objectid`);
-                        objectcode = shareLink.getAttribute(`objectcode`);
+                        if (shareLink !== null) {
+                            albumID = shareLink.getAttribute(`objectid`);
+                            objectcode = shareLink.getAttribute(`objectcode`);
+                        }
                     }
                 }
             }
-            else if (e.target.tagName === `DIV` && !e.target.classList.contains(`leftMenuPanelItemRow__title--1`)) {
-                albumID = e.target.parentNode.parentNode.getAttribute(`object-id`); // SG3
+            else if (e.target.tagName === `DIV` && !e.target.classList.contains(`leftMenuPanelItemRow__title--1`)) { // SG3
+                albumID = e.target.parentNode.parentNode.getAttribute(`object-id`);
             }
             else { // Item clicked was not an album item
                 return; // eslint-disable-line no-useless-return
@@ -385,13 +448,46 @@
 
             // -----------------------------------------------------------------
 
+            else if (ß.data.MODE === `LIVE`) {
+                requestPromise = fetch(`${document.location.origin}/content/album.aspx?${ß.makeArgString(ß.data.reqArgs)}`, {
+                    body: null,
+                    cache: `force-cache`,
+                    method: GET
+                })
+                    .then((txt) => txt.text())
+                    .then((html) => {
+                        ß.data.MOREINFO_HTML = ß.makeFragmentFromString(html);
+                    })
+                    .then(() => eval(ß.data.MOREINFO_HTML.querySelector(`#spanJSON_album`).textContent).playlist_all) // eslint-disable-line no-eval
+                    .then((json) => {
+                        const relData = json[0].collectionname.match(/^(\w+)\s(.+)/u);
+
+                        Object.defineProperties(ß.data, {
+                            ALBUM_JSON: {
+                                value: Object.freeze({
+                                    albumCode: relData[1],
+                                    albumLibraryName: json[0].cataloguename,
+                                    albumName: relData[2]
+                                }),
+                                writable: true
+                            },
+                            TRACK_JSON: {
+                                value: Object.freeze(json),
+                                writable: true
+                            }
+                        });
+                    });
+            }
+
+            // -----------------------------------------------------------------
+
             else if (ß.data.MODE === `SG2`) {
                 requestPromise = fetch(`${document.location.origin}//Handler/GetTracksPostMethod.ashx`, {
                     headers: {
                         'content-type': `application/x-www-form-urlencoded; charset=UTF-8`
                     },
                     body: `type=album_tracks&id=${albumID}&isheader=false`,
-                    method: `POST`
+                    method: POST
                 })
                     .then((txt) => txt.text())
                     .then((txt) => txt.split(`{|}`))
@@ -419,7 +515,7 @@
                 if (typeof objectcode === `undefined`) { // generic SG3 sites
                     requestPromise = fetch(`${document.location.origin}/Handler/GetAlbum.ashx`, {
                         body: `{"id":"${albumID}"}`,
-                        method: `POST`
+                        method: POST
                     })
                         .then((txt) => txt.json())
                         .then((json) => {
@@ -435,11 +531,14 @@
                             });
                         });
                 }
+
+                // -----------------------------------------------------------------
+
                 else { // West One Music, (others?)
                     requestPromise = fetch(`${document.location.origin}/Handlers/GetAlbum.ashx`, {
                         body: `albumCode=${objectcode}&libraries=${encodeURI(libraries)}`,
                         headers: new Headers({'content-type': `application/x-www-form-urlencoded;charset=UTF-8`}),
-                        method: `POST`
+                        method: POST
                     })
                         .then((txt) => txt.text())
                         .then((txt) => {
@@ -461,12 +560,7 @@
 
             // -----------------------------------------------------------------
 
-            else {
-                displayError(`Unknown mode for site.`);
-                ß.data.errorState = 1;
-
-                return; // eslint-disable-line no-useless-return
-            }
+            else setBadMode(); // eslint-disable-line curly
 
             // -----------------------------------------------------------------
 
@@ -484,14 +578,45 @@
         });
     };
 
+    // Sets any site-specific info needed to start doing fetch requests
+    const getSiteSettings = () => {
+        // -----------------------------------------------------------------
+
+        if (ß.data.MODE === `LIVE`) {
+            ß.data.reqArgs = {
+                acctid: document.querySelector(`#acctid`).value,
+                macctid: document.querySelector(`#macctid`).value,
+                sorttype: `MY`
+            };
+        }
+
+        // -----------------------------------------------------------------
+
+        else if (ß.data.MODE === `SG2`) {
+            // None currently
+        }
+
+        // -----------------------------------------------------------------
+
+        else if (ß.data.MODE === `SG3`) {
+            // None currently
+        }
+
+        // -----------------------------------------------------------------
+
+        else setBadMode(); // eslint-disable-line curly
+
+        // -----------------------------------------------------------------
+    };
+
     (function loader () {
         ß.data.MODE = ((test) => {
             switch (test) {
+            case typeof auditionOnly === `function`:
+                return `LIVE`;
             case typeof extraSettingsToPost === `string`:
                 return `SG2`;
-            case typeof preMeta === `string`:
-                return `SG3`;
-            case typeof SG3_Config === `object`:
+            case typeof preMeta === `string` || typeof SG3_Config === `object`:
                 return `SG3`;
             default:
                 return `unknown`;
@@ -499,11 +624,12 @@
         })(true);
 
         const waitForMenu = async () => {
-                const noLibraryVar = document.querySelector(`#librariesSortable, #leftNavContainer`),
+                const noLibraryVar = document.querySelector(`#librariesSortable, #leftNavContainer, .jspPane`),
                     hasLibraryVar = document.querySelector(`.searchlanding`);
 
                 if (noLibraryVar !== null || (hasLibraryVar !== null && typeof libraries === `string`)) {
                     clearInterval(checker); // eslint-disable-line no-use-before-define
+                    getSiteSettings();
                     await populateLabelDB();
                     setListener(noLibraryVar || hasLibraryVar);
                 }
